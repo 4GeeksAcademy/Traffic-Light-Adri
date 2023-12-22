c94f413e68a2ed19c0a74ab85830d87ad2d4932b
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-12-22 20:18:17 +0000

    Ready

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,58 @@
 import React, { useState } from "react";
 
-
-
-//create your first component
 const Home = () => {
-	const [color, setColor] = useState();
-	
-	
-	return (
-		<div className="container ">
-			<div className="palo d-flex  justify-content-center"></div>
-			<div className="semaforo">
-				<h1 onClick={() => setColor("red")} className={"circulo " + ( color==="red" ? "selected" : "")} id="red"></h1>
-				<h1 onClick={() => setColor("yellow")} className={"circulo " + ( color==="yellow" ? "selected" : "")} id="yellow"></h1>
-				<h1 onClick={() => setColor("green")} className={"circulo " + ( color==="green" ? "selected" : "")} id="green"></h1>
-			</div>
-		</div>
-	);
+  const [color, setColor] = useState();
+  const [bailando, setBailando] = useState(false);
+
+  const handleDanceClick = () => {
+    setBailando(!bailando);
+  };
+
+  const handleRandomLightClick = () => {
+    const colors = ["red", "yellow", "green"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(randomColor);
+  };
+
+  return (
+    <div className="container">
+      <div className="palo d-flex justify-content-center"></div>
+      <div className="semaforo">
+        <h1
+          onClick={() => setColor("red")}
+          className={`circulo ${color === "red" ? "selected" : ""} ${
+            bailando ? "bailando" : ""
+          }`}
+          id="red"
+        ></h1>
+        <h1
+          onClick={() => setColor("yellow")}
+          className={`circulo ${color === "yellow" ? "selected" : ""} ${
+            bailando ? "bailando" : ""
+          }`}
+          id="yellow"
+        ></h1>
+        <h1
+          onClick={() => setColor("green")}
+          className={`circulo ${color === "green" ? "selected" : ""} ${
+            bailando ? "bailando" : ""
+          }`}
+          id="green"
+        ></h1>
+      </div>
+      <div className="container d-flex justify-content-evenly mt-5 p-5">
+        <button className="btn btn-primary w-25" onClick={handleDanceClick}>
+          Dance
+        </button>
+        <button
+          className="btn btn-primary w-25"
+          onClick={handleRandomLightClick}
+        >
+          RandomLigth
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
